765d0fb7a93521a72d572bb346c1813252eb7987
nicolaskjh <nicolas.koh.jh@gmail.com>
2024-05-27 12:14:01 +0800

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 flex-col justify-between bg-black">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
